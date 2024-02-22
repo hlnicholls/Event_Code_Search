077bd02206d9e312c662dc5f8989c9d9a0cba59c
hlnicholls <hannahnicholls@qmul.ac.uk>
2024-02-22 11:13:43 +0000

    Feat: updated arguments for txt file

--- README.md
+++ README.md
@@ -1,9 +1,49 @@
 # Event_Code_Search
-UKBB ICD code and event search tool.
+UK Biobank code and event search tool.
+
+## Installation:
+Tested with Python version 3.9
+
+It is recommended that you first create a virtual environment before installing
+
+1. Clone/download this repository to your local computer.
+2. In the terminal change the work directory to the repository's local lcoation.
+
+```
+cd  /user_path/event_code_search/
+```
+
+3. Activate/create your virtual environment
+e.g. with conda:
+``` 
+conda create -n event_code_search python=3.9 
+
+conda activate event_code_search
+```
+
+or with virtualenv
+
+ ```
+python3.9 -m venv /your_path/event_code_search
+ 
+source /your_path/event_code_search/bin/activate
+ ```
+
+3. Pip install the package from its directory
+
+```
+pip install -e .
+```
+
+
+## Running:
 
 ```
 event_code_search --dat_path /path_to_outcome_data.csv --pkl_path /path_to_event_data.pkl --output_path /path_to_output --icd10_codes I10 I11 --all_causes_death
 ```
+
+All code arguments in the command can either be entered as a space-separated list, or if you have a longer list of codes you can use a text file (one code per row in the file):
+```--icd10_codes /path_to_codes/icd10_codes.txt```
 
 
 ```
@@ -14,19 +54,19 @@ usage: event_code_search [-h] --dat_path DAT_PATH --pkl_path PKL_PATH --output_p
 
 Process and analyze medical event data from the UK Biobank.
 
-options:
+optional arguments:
   -h, --help            show this help message and exit
   --dat_path DAT_PATH   Path to the CSV data file containing the dataset.
   --pkl_path PKL_PATH   Path to the pickle file containing preloaded event data.
   --output_path OUTPUT_PATH
                         Directory path for saving the output files.
   --icd10_codes ICD10_CODES [ICD10_CODES ...]
-                        List of ICD10 codes of interest. Space-separated if multiple.
+                        List of ICD10 codes of interest or path to text file containing codes. Space-separated if multiple or a single path to a file.
   --icd9_codes ICD9_CODES [ICD9_CODES ...]
-                        List of ICD9 codes of interest. Space-separated if multiple.
+                        List of ICD9 codes of interest or path to text file containing codes. Space-separated if multiple or a single path to a file.
   --death_icd10_codes DEATH_ICD10_CODES [DEATH_ICD10_CODES ...]
-                        List of Death-related ICD10 codes of interest. Space-separated if multiple.
+                        List of Death-related ICD10 codes of interest or path to text file containing codes. Space-separated if multiple or a single path to a file.
   --opcs4_codes OPCS4_CODES [OPCS4_CODES ...]
-                        List of OPCS4 codes of interest. Space-separated if multiple.
+                        List of OPCS4 codes of interest or path to text file containing codes. Space-separated if multiple or a single path to a file.
   --all_causes_death    Flag to include all-cause mortality in the search.
   ```
--- event_code_search/config_class.py
+++ event_code_search/config_class.py
@@ -13,11 +13,8 @@ class Config:
         self.opcs4_codes = opcs4_codes
         self.all_causes_death = all_causes_death
 
-        try:
-            os.makedirs(self.output_path, exist_ok=True)
-        except FileExistsError:
-            pass
-
+        # Ensure output directory exists
+        os.makedirs(self.output_path, exist_ok=True)
 
     @staticmethod
     def create_from_args():
@@ -25,16 +22,36 @@ class Config:
         parser.add_argument('--dat_path', type=Path, required=True, help='Path to the CSV data file containing the dataset.')
         parser.add_argument('--pkl_path', type=Path, required=True, help='Path to the pickle file containing preloaded event data.')
         parser.add_argument('--output_path', type=Path, required=True, help='Directory path for saving the output files.')
-        parser.add_argument('--icd10_codes', type=str, nargs='+', default=[], help='List of ICD10 codes of interest. Space-separated if multiple.')
-        parser.add_argument('--icd9_codes', type=str, nargs='+', default=[], help='List of ICD9 codes of interest. Space-separated if multiple.')
-        parser.add_argument('--death_icd10_codes', type=str, nargs='+', default=[], help='List of Death-related ICD10 codes of interest. Space-separated if multiple.')
-        parser.add_argument('--opcs4_codes', type=str, nargs='+', default=[], help='List of OPCS4 codes of interest. Space-separated if multiple.')
+        parser.add_argument('--icd10_codes', type=str, nargs='+', default=[], help='List of ICD10 codes of interest or path to text file containing codes. Space-separated if multiple or a single path to a file.')
+        parser.add_argument('--icd9_codes', type=str, nargs='+', default=[], help='List of ICD9 codes of interest or path to text file containing codes. Space-separated if multiple or a single path to a file.')
+        parser.add_argument('--death_icd10_codes', type=str, nargs='+', default=[], help='List of Death-related ICD10 codes of interest or path to text file containing codes. Space-separated if multiple or a single path to a file.')
+        parser.add_argument('--opcs4_codes', type=str, nargs='+', default=[], help='List of OPCS4 codes of interest or path to text file containing codes. Space-separated if multiple or a single path to a file.')
         parser.add_argument('--all_causes_death', action='store_true', help='Flag to include all-cause mortality in the search.')
+        
         args = parser.parse_args()
+
+        # Function to determine if a path is a file or not
+        def is_file_path(p):
+            return Path(p).is_file()
+
+        # Helper function to process codes input
+        def process_codes_input(codes_input):
+            # If the input is a single item and it's a file path, read the codes from the file
+            if len(codes_input) == 1 and is_file_path(codes_input[0]):
+                with open(codes_input[0]) as f:
+                    return [line.strip() for line in f if line.strip()]
+            # Otherwise, treat it as a list of codes
+            return codes_input
+
+        # Process codes input
+        icd10_codes = process_codes_input(args.icd10_codes)
+        icd9_codes = process_codes_input(args.icd9_codes)
+        death_icd10_codes = process_codes_input(args.death_icd10_codes)
+        opcs4_codes = process_codes_input(args.opcs4_codes)
 
         return Config(
             dat_path=args.dat_path, pkl_path=args.pkl_path, output_path=args.output_path,
-            icd10_codes=args.icd10_codes, icd9_codes=args.icd9_codes,
-            death_icd10_codes=args.death_icd10_codes, opcs4_codes=args.opcs4_codes,
+            icd10_codes=icd10_codes, icd9_codes=icd9_codes,
+            death_icd10_codes=death_icd10_codes, opcs4_codes=opcs4_codes,
             all_causes_death=args.all_causes_death
         )
